d3821eafec43be351c1332d06323fa9fc41e4116
damiaNR10 <damianrog96@gmail.com>
2020-06-12 13:01:07 +0200

    homework w8 done

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import '../scss/main.scss';
+import moment from 'moment';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log('HELLO 🚀');
+console.log(moment().startOf('day').fromNow());
